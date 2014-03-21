93980fa5cbafbbef86c90fcfca8eda752c8d2713
Ashley Rae Lee & Patti Jurgensen <student@epicodus.com>
2014-03-21 12:27:44 -0700

    added category and company spec, added category and company to databases

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
 require 'rspec'
 require 'pg'
 require 'purchase'
+require 'category'
+require 'company'
 
 DB = PG.connect(:dbname => 'expense_organizer_test')
 
 RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM purchase *;")
+    DB.exec("DELETE FROM category *;")
+    DB.exec("DELETE FROM company *;")
   end
 end
